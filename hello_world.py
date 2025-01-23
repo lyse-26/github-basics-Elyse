fe6b92ecbc285db1e328cd2a7b5fe827762cb1c7
lyse-26 <978908@hazelwoodschools.org>
2025-01-23 07:35:29 -0600

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,6 +1,9 @@
+
+#this is the varible for tracking number of loops
 loop_count = 0
 
+#the loop that will print the message 
 while loop_count >= 3:
   print("Hello world")
-  loop_count +1
+  loop_count +1 #increases the loop count
   
