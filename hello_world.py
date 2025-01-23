4014935a61f7cd2c9a1f9c62eaaf170ff0cb6fc8
lyse-26 <978908@hazelwoodschools.org>
2025-01-23 07:47:26 -0600

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,6 +1,4 @@
-#this is the varible for tracking number of loops
-loop_count = 0
 
 #the loop that will print the message 
-for i in range(3):
+for loop_count in range(3):#three is the amount of times the message will print
   print("Hello world")
